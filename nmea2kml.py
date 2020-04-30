f3fba81f0a044e1d1edad76052ca3678c4654178
c-yan <c-yan@users.noreply.github.com>
2020-04-30 13:34:57 +0900

    output placemarks every minute

--- nmea2kml.py
+++ nmea2kml.py
@@ -15,13 +15,24 @@ template_before = '''<?xml version="1.0" encoding="UTF-8"?>
       <width>3</width>
     </LineStyle>
   </Style>
+  <Style id="blueDot">
+    <IconStyle>
+      <Icon>
+        <href>http://maps.gstatic.com/mapfiles/ms2/micons/blue-dot.png</href>
+      </Icon>
+    </IconStyle>
+  </Style>
   <Placemark>
     <name>Route</name>
     <styleUrl>#roadStyle</styleUrl>
     <MultiGeometry>\n'''
 
-template_after = '''    </MultiGeometry>
+template_middle = '''    </MultiGeometry>
   </Placemark>
+  <Folder>
+    <name>waypoints</name>\n'''
+
+template_after = '''  </Folder>
 </Document>
 </kml>\n'''
 
@@ -36,12 +47,23 @@ def convert(input):
         t = s.rstrip('\r\n').split(',')
         if len(t) != 15 or t[0] != '$GPGGA' or t[2] == '' or t[4] == '' or t[9] == '':
             continue
-        result.append('%.7f,%.7f,%s' % (to_google(t[4]), to_google(t[2]), t[9]))
+        result.append((t[1], to_google(t[4]), to_google(t[2]), t[9]))
     return result
 
 def write_output(points):
     stdout.write(template_before)
-    stdout.write('      <LineString><coordinates>%s</coordinates></LineString>\n' % ' '.join(points))
+    stdout.write('      <LineString><coordinates>%s</coordinates></LineString>\n' % ' '.join('%.7f,%.7f,%s' % p[1:] for p in points))
+    stdout.write(template_middle)
+    t = ''
+    for p in points:
+        if p[0][:4] == t:
+            continue
+        t = p[0][:4]
+        stdout.write('    <Placemark>\n')
+        stdout.write('      <name>%s</name>\n' % ('%s:%s' % (t[:2], t[2:])))
+        stdout.write('      <styleUrl>#blueDot</styleUrl>\n')
+        stdout.write('      <Point><coordinates>%s</coordinates></Point>\n' % ('%.7f,%.7f,%s' % p[1:]))
+        stdout.write('    </Placemark>\n')
     stdout.write(template_after)
 
 def main(argv):
